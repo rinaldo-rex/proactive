3213a3ef9fe95212ee8bd23a8f7902721be90101
Rinaldo Rex <rinaldorex94@gmail.com>
2017-12-03 00:39:49 +0530

    Listing task resolved through dict.get() method

--- proactive.py
+++ proactive.py
@@ -28,22 +28,57 @@ class TaskSet:
             with open(file_path, 'r+') as self.file:
                 self.data = json.load(self.file)
 
+    def sync(self):
+        with open(file_path, 'w') as file:
+            json.dump(self.data, file)
+
 pass_taskset = click.make_pass_decorator(TaskSet, ensure=True)
 
 
 @click.group()
 @pass_taskset
 def cli(taskset):
-    """A simple command-line todo application"""
+    """A simple command-line todo application\n
+       Intended to help(force, actually) kill procrastination.
+       Or better, intended to get you to habituate acting over thinking!\n
+       ஐந்தில் வளையாதது ஐம்பதில் வளையாது
+    """
 
 
 @cli.command()
 @pass_taskset
-# @click.option('-l', '--list')
+# TODO:List tasks based on category, tags, deadlines
 def list(taskset):
+    """Lists available tasks to ACT on"""
     if len(taskset.data) <= 1:
         click.echo("No tasks - Hurray!")
     else:
         for task in taskset.data:
-            click.secho(task['name'], fg='green', blink=True, bold=True)
-        taskset.file.close()
+            click.echo(task.get('name')) # Why does dict.get(key) works and not dict[key]?
+        # taskset.file.close()
+
+@cli.command()
+@pass_taskset
+@click.option('-n', '--name', 'name', help="Name/Title of the task")
+@click.option('-l', '--length', 'length', help="How long does this task take?(xhrs/xdays)")
+# TODO: Add humanized date methods
+@click.option('-d', '--due', 'due', type=(int, str), help="When's the deadline?(DD MMM) (Eg: 2 mar")
+@click.option('-c', '--category', 'category', default='personal', help="Classify the task under a category")
+@click.option('-t', '--tags', 'tags', default='misc', help="Classify the task under a tag")
+def add(taskset, name, length, due, category, tags):
+    """Let's add something to ACT upon"""
+    task = {} # a task is a dictionary with at least name, length and deadline.
+    id = len(taskset.data) + 2
+    task['id'] = id
+    if not name:
+        click.echo("Really? You wanna do nothing?")
+    if name and length and due:
+        task['name'] = name
+        task['length'] = length
+        date = str(due[0]).zfill(2) + ' ' + due[1] + ' ' + str(arrow.utcnow().year)
+        date1 = arrow.get(date, 'DD MMM YYYY').timestamp
+        task['due'] = date1
+
+    click.echo(task)
+    taskset.data.append(task)
+    taskset.sync()
