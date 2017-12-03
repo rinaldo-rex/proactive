f3fa3651ae03992f6123c98935da6cdeeab8b3e0
Rinaldo Rex <rinaldorex94@gmail.com>
2017-12-03 21:32:48 +0530

    Listing based on task length done

--- proactive.py
+++ proactive.py
@@ -4,7 +4,8 @@ import arrow   # for date handling (due/deadline)
 import json    # for parsing tasks.json file; contains all the tasks.
 import os      # for handling file manipulations. (saving and deleting tasks)
 import shutil  # for copying the tasks.json file into appconfig directory.
-from collections import deque # for manipulations on tasks.json
+import math    # for ceiling user task lengths
+from collections import deque  # for manipulations on tasks.json
 
 file_dir = click.get_app_dir('proactive')
 file_path = os.path.join(file_dir, 'tasks.json')
@@ -29,6 +30,7 @@ class TaskSet:
             with open(file_path, 'r+') as self.file:
                 self.data = json.load(self.file)
         self.sort()
+
     def sync(self):
         with open(file_path, 'w') as file:
             json.dump(self.data, file)
@@ -41,7 +43,7 @@ class TaskSet:
         # a = list(self.data)
         # print(a)
         tasks = deque(self.data)
-        tasks.popleft() # remove the tutorial content from tasks.json
+        tasks.popleft()  # remove the tutorial content from tasks.json
         for item in tasks:
             self.tasks.append(item)
         # inline sorting of the list of tasks in ascending order of due dates
@@ -66,28 +68,50 @@ def cli(taskset):
 @cli.command()
 @pass_taskset
 # TODO:List tasks based on category, tags, deadlines
-@click.option('-l', '--length', 'length', type=str, help="How long do you have?(xm/xh/xd)")
+@click.option('-l', '--length', 'length', type=float, help="How long do you have in hours?(xh/x.yh)")
 def list(taskset, length):
     """Lists available tasks to ACT on"""
     # sorted_taskset = taskset.sort()
-    if len(taskset.tasks) <= 1:
-        click.echo("No tasks - Hurray!")
-    else:
+    next_task = {}  # sort tasks based on length(and category/tags if specified)
+    tasks = []
+    if length:
         for task in taskset.tasks:
-            click.echo(task.get('name'))# Why does dict.get(key) work and not dict[key]?
-        # taskset.file.close()
+            if math.floor(float(task['length'])) <= math.ceil(length):
+                tasks.append(task)
+                # tasks.remove(tasks.index(task)) # again... no idea why this doesn't work. Maybe json parsing issue? :/
+                # taskset.tasks.remove(taskset.tasks.index(task))
+                # print(taskset.tasks.index(task))
+        # print(tasks)
+    if len(taskset.tasks) == 0:
+        click.echo("No tasks - Hurray!")
+    elif len(tasks) == 0:
+        if click.confirm("Uh oh... We have tasks, but shall take longer. Wanna take a peek?"):
+            for task in taskset.tasks:
+                if taskset.tasks.index(task) == 0:
+                    click.secho(task['name'], fg='blue')
+                else:
+                    click.echo(task['name'])
+    else:
+        for task in tasks:
+            t = "%s -- %s due on %s" % (task['name'], task['length'], arrow.get(task['due']).format('DD-MMM'))
+            if tasks.index(task) == 0:
+                click.secho(t, fg='green')  # Why does dict.get(key) work and not dict[key]?
+            else:
+                click.secho(t, fg='blue')
+        # taskset.file.close() # not necessary because 'with open' auto closes! :)
+
 
 @cli.command()
 @pass_taskset
 @click.option('-n', '--name', 'name', help="Name/Title of the task")
-@click.option('-l', '--length', 'length', help="How long does this task take?(xm/xh/xd as in minutes/hours/days)")
-# TODO: Add humanized date methods
+@click.option('-l', '--length', 'length', type=float, help="How long does this task take in hours?(xh or x.yh)")
+# TODO: This date method works for the current year only. Need to resolve.
 @click.option('-d', '--due', 'due', type=(int, str), help="When's the deadline?(DD MMM) (Eg: 2 mar")
 @click.option('-c', '--category', 'category', default='personal', help="Classify the task under a category")
 @click.option('-t', '--tags', 'tags', default='misc', help="Classify the task under a tag")
 def add(taskset, name, length, due, category, tags):
     """Let's add something to ACT upon"""
-    task = {} # a task is a dictionary with at least name, length and deadline.
+    task = {}  # a task is a dictionary with at least name, length and deadline.
     id = len(taskset.data) + 2
     task['id'] = id
     # if not name:
