d8cce96b4003ad2941d1564aabadce27635114f0
Rinaldo Rex <rinaldorex94@gmail.com>
2017-12-03 21:53:41 +0530

    Fair adding and listing done

--- proactive.py
+++ proactive.py
@@ -16,11 +16,10 @@ class TaskSet:
     """The list of tasks at hand"""
     # TODO: --hard-reset option to generate a new tasks.json
     def __init__(self):
-        # self.data = [{"id": 0}]
         self.file = None
         self.tasks = []
         if not os.path.exists(file_path):
-            click.secho(f"tasks.json file doesn't exist at {file_path}, Creating...", dim=True)
+            click.secho("tasks.json file doesn't exist at %s, Creating..." % file_path, dim=True)
             os.makedirs(file_dir, exist_ok=True)
             if not os.path.exists(file_path):
                 shutil.copy('tasks.json', file_dir)
@@ -37,20 +36,12 @@ class TaskSet:
 
     def sort(self):
         """Initially sort based on deadline"""
-        # print(type(self.data))
-        # a = [self.data]
-        # print(a)
-        # a = list(self.data)
-        # print(a)
         tasks = deque(self.data)
         tasks.popleft()  # remove the tutorial content from tasks.json
         for item in tasks:
             self.tasks.append(item)
         # inline sorting of the list of tasks in ascending order of due dates
         self.tasks.sort(key=lambda task: task['due'], reverse=False)
-        # print(self.tasks)
-        # self.tasks = list(tasks)  # I honestly have no idea why this doesn't work! :(
-        # print(type(self.tasks), self.tasks)
 
 pass_taskset = click.make_pass_decorator(TaskSet, ensure=True)
 
@@ -72,33 +63,49 @@ def cli(taskset):
 def list(taskset, length):
     """Lists available tasks to ACT on"""
     # sorted_taskset = taskset.sort()
-    next_task = {}  # sort tasks based on length(and category/tags if specified)
     tasks = []
     if length:
         for task in taskset.tasks:
             if math.floor(float(task['length'])) <= math.ceil(length):
                 tasks.append(task)
-                # tasks.remove(tasks.index(task)) # again... no idea why this doesn't work. Maybe json parsing issue? :/
-                # taskset.tasks.remove(taskset.tasks.index(task))
-                # print(taskset.tasks.index(task))
-        # print(tasks)
     if len(taskset.tasks) == 0:
         click.echo("No tasks - Hurray!")
     elif len(tasks) == 0:
         if click.confirm("Uh oh... We have tasks, but shall take longer. Wanna take a peek?"):
             for task in taskset.tasks:
-                if taskset.tasks.index(task) == 0:
-                    click.secho(task['name'], fg='blue')
+                blink_flag = False
+                t = "(ID:%s) %s needs %s hr(s) due on %s" % (task['id'],
+                                                             task['name'],
+                                                             task['length'],
+                                                             arrow.get(task['due']).format('DD-MMM'))
+                if arrow.get(task['due']).format('DD-MMM') <= arrow.now().format('DD-MMM'):
+                    fg_color = 'red'
+                    if arrow.get(task['due']).format('DD-MMM') == arrow.now().format('DD-MMM'):
+                        blink_flag = True
                 else:
-                    click.echo(task['name'])
+                    fg_color = 'blue'
+                if taskset.tasks.index(task) == 0:
+                    click.secho(t, fg=fg_color, blink = blink_flag)
+                else:
+                    click.echo(t)
     else:
         for task in tasks:
-            t = "%s -- %s due on %s" % (task['name'], task['length'], arrow.get(task['due']).format('DD-MMM'))
+            blink_flag = False
+            t = "(ID:%s) %s needs %s hr(s) due on %s" % (task['id'],
+                                               task['name'],
+                                               task['length'],
+                                               arrow.get(task['due']).format('DD-MMM'))
+            if arrow.get(task['due']).format('DD-MMM') <= arrow.now().format('DD-MMM'):
+                fg_color = 'red'
+                if arrow.get(task['due']).format('DD-MMM') == arrow.now().format('DD-MMM'):
+                    blink_flag = True
+            else:
+                fg_color = 'green'
+
             if tasks.index(task) == 0:
-                click.secho(t, fg='green')  # Why does dict.get(key) work and not dict[key]?
+                click.secho(t, fg=fg_color, bold=True, blink=blink_flag)
             else:
                 click.secho(t, fg='blue')
-        # taskset.file.close() # not necessary because 'with open' auto closes! :)
 
 
 @cli.command()
@@ -106,6 +113,7 @@ def list(taskset, length):
 @click.option('-n', '--name', 'name', help="Name/Title of the task")
 @click.option('-l', '--length', 'length', type=float, help="How long does this task take in hours?(xh or x.yh)")
 # TODO: This date method works for the current year only. Need to resolve.
+# Made available only for current year to support the philosophy. (This is not for long-term tasks)
 @click.option('-d', '--due', 'due', type=(int, str), help="When's the deadline?(DD MMM) (Eg: 2 mar")
 @click.option('-c', '--category', 'category', default='personal', help="Classify the task under a category")
 @click.option('-t', '--tags', 'tags', default='misc', help="Classify the task under a tag")
@@ -123,6 +131,5 @@ def add(taskset, name, length, due, category, tags):
         date1 = arrow.get(date, 'DD MMM YYYY').timestamp
         task['due'] = date1
 
-    # click.echo(task)
     taskset.data.append(task)
     taskset.sync()
